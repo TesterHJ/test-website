2d6e71a1ccbaf5a41b8997984a0ad54afd27ba6b
TesterHJ <hjyoon31@naver.com>
2026-05-17 23:07:56 +0900

    Delete js directory

--- js/auth.js
+++
@@ -1,8 +0,0 @@
-function login() {
-  const clientId = "Ov23li9ERM0fBAwVc3Wo";
-  const redirectUri = "https://testerhj.github.io/test-website/";
-  const scope = "repo";
-
-  window.location.href =
-    `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
-}
