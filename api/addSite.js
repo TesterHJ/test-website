3524b8b402881c9ab4744e9ce15019c529d266d2
TesterHJ <hjyoon31@naver.com>
2026-06-03 04:25:20 +0900

    Add files via upload

---
+++ api/addSite.js
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+export default async function handler(req, res) {
+  try {
+    await client.connect();
+    const db = client.db("myAdminDB");
+    const sitescollection = db.collection("sites");
+
+    const { siteName, siteUrl, siteCategory, siteIcon,  siteDescription} = req.body;
+
+    if (!siteName || !siteUrl || !siteCategory || !siteIcon) {
+      return res.status(400).json({ error: "사이트 이름, 주소, 카테고리, 아이콘을 모두 입력해주세요." });
+    }
+
+    await sitescollection.insertOne({
+      name: siteName,
+      url: siteUrl,
+      category: siteCategory,
+      icon: siteIcon || "fa-solid fa-globe",
+      description: siteDescription || ""
+    });
+
+    res.status(200).json({ message: "사이트 추가 완료" });
+  } catch (error) {
+    res.status(500).json({ error: "DB 저장 실패" });
+  }
+}
